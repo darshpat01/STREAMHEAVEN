7d12812e7ac636a39a97a6bc1dfb13ee3fbd28bc
darshan <darshpat01@gmail.com>
2023-03-12 04:04:22 +0530

    navbar prototype complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Navbar />
     </div>
   );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,8 +1,12 @@
 import React from "react";
 import NavItem from "./NavItem";
+import { SiRiotgames } from "react-icons/si";
+import { BsSearch } from "react-icons/bs";
+import { AiOutlineMessage, AiOutlineBell } from "react-icons/ai";
+import { InputGroup, InputLeftElement, Input } from "@chakra-ui/react";
 
 const navitemStyle = "text-navItemColor text-sm";
-const navItemTextStyle = "hover:text-navItemColor text-sm";
+const navItemTextStyle = "hover:text-navItemColor text-sm uppercase";
 
 function Navbar() {
   const handleClickScroll = (Id) => {
@@ -13,45 +17,56 @@ function Navbar() {
   };
   return (
     <div className="fixed w-[100%] z-40 bg-opacity-10 backdrop-blur-sm ">
-      <ul className="flex flex-row container mx-auto xl:py-2 ">
-        <li className=" p-1 m-1 font-solarscript text-2xl pr-4 ">
-          Darshan Patel
-        </li>
-        <NavItem className="ml-auto" onClick={() => handleClickScroll("About")}>
+      <ul className="flex flex-row container mx-auto xl:py-2 items-center">
+        <div className="flex items-center">
+          <span className="pr-3">
+            <SiRiotgames size={20} />
+          </span>
+          <li className=" p-1 m-1 text-2xl pr-4 uppercase">CipherSchools</li>
+        </div>
+
+        <NavItem className="auto" onClick={() => handleClickScroll("About")}>
           <div className="group">
-            <span className={navitemStyle}>01.</span>{" "}
-            <span className={navItemTextStyle}>About</span>
+            <span className={navItemTextStyle}>Top streaming</span>
           </div>
         </NavItem>
         <NavItem onClick={() => handleClickScroll("ExperienceList")}>
           <div className="group">
-            <span className={navitemStyle}>02.</span>{" "}
-            <span className={navItemTextStyle}>Experience</span>
+            <span className={navItemTextStyle}>games</span>
           </div>
         </NavItem>
         <NavItem onClick={() => handleClickScroll("Projects")}>
           <div className="group">
-            <span className={navitemStyle}>03.</span>{" "}
-            <span className={navItemTextStyle}>Project</span>
+            <span className={navItemTextStyle}>teams</span>
           </div>
         </NavItem>
-        <NavItem onClick={() => handleClickScroll("Contact")}>
-          <div className="group">
-            <span className={navitemStyle}>04.</span>{" "}
-            <span className={navItemTextStyle}>Contact</span>
+        <NavItem
+          onClick={() => handleClickScroll("Projects")}
+          className="pl-11 w-[30%]"
+        >
+          <InputGroup>
+            <InputLeftElement pointerEvents="none" children={<BsSearch />} />
+            <Input type="text" placeholder="Search" />
+          </InputGroup>
+        </NavItem>
+        <NavItem className="ml-auto">
+          <AiOutlineMessage size={20} />
+        </NavItem>
+        <NavItem>
+          <AiOutlineBell size={20} />
+        </NavItem>
+        <NavItem>
+          <p>Darshan</p>
+        </NavItem>
+        <NavItem>
+          <div className="rounded-md border-4 border-purple-500 overflow-hidden">
+            <img
+              className=" h-8 w-8"
+              src="https://prod.assets.earlygamecdn.com/images/tenz-valorfeed-settings.jpg?mtime=1651353441"
+              alt="profile"
+            />
           </div>
         </NavItem>
-        <li className="font-opensans  p-2 px-3 m-1 text-navItemColor border rounded-md border-navItemColor  hover:bg-[#9966cc1a]">
-          <span className="font-semibold text-sm">
-            <a
-              href="https://drive.google.com/file/d/1NW7SgSsC0opyEX_YEKpYWiFRTXcBgEOt/view?usp=sharing"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Resume
-            </a>
-          </span>
-        </li>
       </ul>
     </div>
   );
