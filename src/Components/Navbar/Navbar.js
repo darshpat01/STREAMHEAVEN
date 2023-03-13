3d33df9454905bc5f0286d5f4675dfefcac7fe61
darshan <darshpat01@gmail.com>
2023-03-13 11:50:46 +0530

    added notification and chat dropdown

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,11 +3,24 @@ import NavItem from "./NavItem";
 import { SiRiotgames } from "react-icons/si";
 import { BsSearch } from "react-icons/bs";
 import { AiOutlineMessage, AiOutlineBell } from "react-icons/ai";
-import { InputGroup, InputLeftElement, Input } from "@chakra-ui/react";
+import { BsDot } from "react-icons/bs";
+import {
+  InputGroup,
+  InputLeftElement,
+  Input,
+  Menu,
+  MenuItem,
+  MenuList,
+  MenuDivider,
+  MenuButton,
+  Button,
+  IconButton,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
-const navitemStyle = "text-navItemColor text-sm";
-const navItemTextStyle = "hover:text-navItemColor text-sm uppercase";
+// add underline hover effect to navItemStyle with start to end animation
+const navItemTextStyle =
+  " text-sm font-semibold text-gray-500 hover:underline hover:text-gray-700 transition duration-300 ease-in-out uppercase";
 
 function Navbar() {
   const handleClickScroll = (Id) => {
@@ -45,7 +58,7 @@ function Navbar() {
         </NavItem>
         <NavItem
           onClick={() => handleClickScroll("Projects")}
-          className="pl-11 w-[30%]"
+          className="pl-8 w-[30%]"
         >
           <InputGroup>
             <InputLeftElement pointerEvents="none" children={<BsSearch />} />
@@ -53,10 +66,96 @@ function Navbar() {
           </InputGroup>
         </NavItem>
         <NavItem className="ml-auto">
-          <AiOutlineMessage size={20} />
+          <Menu>
+            <MenuButton
+              as={IconButton}
+              aria-label="Options"
+              variant="outline"
+              colorScheme="purple"
+              icon={<AiOutlineMessage size={20} />}
+              className="flex items-center justify-center outline-none"
+            />
+
+            <MenuList>
+              <MenuItem>
+                <div className="flex items-center">
+                  <p className="text-md text-purple-500 text-md font-semibold">
+                    Subroza: Hi Shawty
+                  </p>
+                  <BsDot size={25} className="text-purple-500 " />
+                </div>
+              </MenuItem>
+              <MenuItem>
+                <div className="flex items-center">
+                  <p className="text-md text-purple-500 text-md font-semibold">
+                    Binks: Kaise hai bhai?
+                  </p>
+                  <BsDot size={25} className="text-purple-500 " />
+                </div>
+              </MenuItem>
+              <MenuItem>
+                <p className="text-md text-gray-500 text-md font-semibold">
+                  Tenz: Lets Colab
+                </p>
+              </MenuItem>
+            </MenuList>
+          </Menu>
         </NavItem>
         <NavItem>
-          <AiOutlineBell size={20} />
+          <Menu>
+            <MenuButton
+              as={IconButton}
+              aria-label="Options"
+              variant="outline"
+              colorScheme="purple"
+              icon={<AiOutlineBell size={20} />}
+              className="flex items-center justify-center outline-none"
+            />
+            <MenuList>
+              <MenuItem>
+                <div>
+                  <p className="font-semibold text-purple-500">
+                    Tenz posted a new video. Check it out now!
+                  </p>
+                  <p className="text-sm text-gray-600 tracking-tighter">
+                    2 hours ago
+                  </p>
+                </div>
+              </MenuItem>
+              <MenuItem>
+                <div>
+                  <p className="font-semibold text-purple-500">
+                    Hiko just went live!
+                  </p>
+                  <p className="text-sm text-gray-600 tracking-tighter">
+                    4 hours ago
+                  </p>
+                </div>
+              </MenuItem>
+
+              <MenuItem>
+                <div>
+                  <p className="font-semibold text-gray-700">
+                    Simple liked your comment!
+                  </p>
+                  <p className="text-sm text-gray-600 tracking-tighter">
+                    2 hours ago
+                  </p>
+                </div>
+              </MenuItem>
+
+              <MenuItem>
+                <div>
+                  <p className="font-semibold text-gray-700 ">
+                    Subscribe to Binks for more Valorant content!
+                  </p>
+                  <p className="text-sm text-gray-600 tracking-tighter">
+                    2 hours ago
+                  </p>
+                </div>
+              </MenuItem>
+            </MenuList>
+          </Menu>
         </NavItem>
         <NavItem>
           <p>Darshan</p>
