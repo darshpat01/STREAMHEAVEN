759a8ac6397c69fc109554e49c56142ac3294806
darshan <darshpat01@gmail.com>
2023-03-13 14:17:37 +0530

    dynamic comments

--- src/Components/Comments/CommentItem.js
+++ src/Components/Comments/CommentItem.js
@@ -3,7 +3,7 @@ import { FiMoreHorizontal } from "react-icons/fi";
 import { BsDot } from "react-icons/bs";
 import { AiOutlineLike, AiOutlineDislike } from "react-icons/ai";
 
-function CommentItem() {
+function CommentItem({ comm, user, likes, dislikes, time }) {
   return (
     <>
       <div className="py-4">
@@ -18,21 +18,21 @@ function CommentItem() {
             </div>
             <div>
               <div className="flex items-center">
-                <p className="text-sm font-semibold pr-1">Senpai</p>
+                <p className="text-sm font-semibold pr-1">{user}</p>
                 <BsDot />
-                <p className="text-sm text-gray-500 pl-1">1 day ago</p>
+                <p className="text-sm text-gray-500 pl-1">{time}</p>
               </div>
               <div>
-                <p className="text-sm text-gray-200 pb-1">Lorem ipsum </p>
+                <p className="text-sm text-gray-200 pb-1">{comm}</p>
               </div>
               <div className="flex">
                 <div className="pr-3 flex items-center">
                   <AiOutlineLike size={20} />
-                  <p className="pl-1 text-sm">325</p>
+                  <p className="pl-1 text-sm">{likes}</p>
                 </div>
                 <div className="pr-3 flex items-center">
                   <AiOutlineDislike size={20} />
-                  <p className="pl-1 text-sm">5</p>
+                  <p className="pl-1 text-sm">{dislikes}</p>
                 </div>
                 <p className="text-sm text-gray-500">Reply</p>
               </div>
--- src/Components/Comments/Comments.js
+++ src/Components/Comments/Comments.js
@@ -7,7 +7,7 @@ function Comments() {
     <>
       <div className="pt-8">
         <div className="flex items-center pb-4">
-          <h1 className="text-lg font-semibold">5 Comments</h1>
+          <h1 className="text-lg font-semibold">4 Comments</h1>
           <div className="ml-auto">
             <h1>
               <span className="text-sm text-gray-500">Sort by</span> Top
@@ -21,10 +21,34 @@ function Comments() {
         />
       </div>
       <div>
-        <CommentItem />
-        <CommentItem />
-        <CommentItem />
-        <CommentItem />
+        <CommentItem
+          comm="Love you and kydae thank you for continuing to upload through this time"
+          user="@sgxeska4466"
+          likes="1.2k"
+          dislikes="10"
+          time="1 day ago"
+        />
+        <CommentItem
+          comm="his is why  i like tenz he is pro  and  humble"
+          user="@GamersCrazyWorld"
+          likes="10k"
+          dislikes="2"
+          time="10 day ago"
+        />
+        <CommentItem
+          comm="What is the sensitivity and aiming line of the tenses?"
+          user="@Senpai"
+          likes="100"
+          dislikes="1"
+          time="1 month ago"
+        />
+        <CommentItem
+          comm="Tenz makes every gun look like a vandal."
+          user="@Kun123"
+          likes="20k"
+          dislikes="10k"
+          time="1 year ago"
+        />
       </div>
     </>
   );
